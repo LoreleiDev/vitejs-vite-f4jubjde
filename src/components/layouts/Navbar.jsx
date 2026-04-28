3c186e35fa5f92c3779d44ca7156aa8deab3507c
Lorelei <145348413+Shikein-Lorelei@users.noreply.github.com>
2026-04-28 13:53:25 +0700

    q

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -72,7 +72,7 @@ export default function Navbar() {
                 <div className="flex justify-between items-center h-16">
 
                     {/* Logo */}
-                    <Link href="/" className="flex items-center space-x-2 group">
+                    <Link to="/" className="flex items-center space-x-2 group">
                         <img
                             src="/logo.webp"
                             alt="Logo"
