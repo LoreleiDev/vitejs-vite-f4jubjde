714ac9040b4a510e2d3c8bf1cddef41fdffc89bd
Lorelei <145348413+Shikein-Lorelei@users.noreply.github.com>
2026-05-27 19:33:59 +0700

    New Update

--- src/components/layouts/ButtonParallax.jsx
+++ src/components/layouts/ButtonParallax.jsx
@@ -51,7 +51,7 @@ export default function ButtonParallax() {
                                     Jasa Pembuatan Website
                                 </h2>
 
-                                <div className="inline-flex items-center gap-1.5 px-4 py-1.5 bg-white text-[#5B23FF] rounded-lg text-xs md:text-sm font-poppins font-semibold shadow-lg group-hover:shadow-xl transition-all duration-300 group-hover:gap-2.5 hover:bg-blue-600 hover:text-white">
+                                <div className="inline-flex items-center gap-1.5 px-4 py-1.5 bg-white text-[#5B23FF] rounded-lg text-xs md:text-sm font-poppins font-semibold shadow-lg group-hover:shadow-xl transition-all duration-300 group-hover:gap-2.5  hover:text-white">
                                     Pesan Sekarang
                                     <svg className="w-3.5 h-3.5 group-hover:translate-x-1 transition-transform" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7l5 5m0 0l-5 5m5-5H6" />
